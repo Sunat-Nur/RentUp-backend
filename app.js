d5a275b780507a007137bf3298c9df2cece5edd8
Sunat_nur <uzbekistan9699@gmail.com>
2024-02-20 12:34:15 +0900

    :feat: config.js file starting script creation

--- app.js
+++ app.js
@@ -1,4 +1,84 @@
-console.log("web serverni boshladik");
+// console.log("web serverni boshladik");
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const router = require("./router");
+// const router_adminka = require("./router_adminka");
+// const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+//
+// let session = require("express-session");
+// const MongoDBStore = require("connect-mongodb-session")(session);
+// const store = new MongoDBStore({
+//     uri: process.env.MONGO_URL,
+//     collection: "session",
+// });
+//
+// //1: Kirish code
+// app.use(express.static("public"));
+// app.use("/uploads", express.static(__dirname + "/uploads"));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.static('js'));
+// app.use(cors({credentials: true, origin: true,}));
+// app.use(cookieParser()); //
+//
+// // 2: Session code
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         cookie: {
+//             maxAge: 1000 * 60 * 59,
+//         },
+//         store: store,
+//         resave: true,
+//         saveUninitialized: true,
+//     })
+// );
+//
+// app.use(function (req, res, next) {
+//     res.locals.member = req.session.member;
+//     next();
+// })
+//
+// // 3: Views code
+// app.set("views", "views");
+// app.set("view engine", "ejs",);
+//
+// //4: routing code
+// app.use("/resto", router_adminka);
+// app.use("/", router);
+//
+//
+// const server = http.createServer(app);
+//
+// const io = require("socket.io")(server, {
+//     serveClient: false,
+//     origin: "*:*",
+//     transport: ["websocket", "xhr-polling"],
+// });
+//
+// let online_users = 0;
+// io.on("connection", function (socket) {
+//     online_users++;
+//     console.log("New user :", online_users);
+//     socket.emit("greetMsg", { text: "Welcome" });
+//     io.emit("infoUsers", { total: online_users });
+//
+//     socket.on("disconnect", function () {
+//         online_users--;
+//         socket.broadcast.emit("infoUsers", { total: online_users });
+//         console.log("client disconnected, total:", online_users);
+//     });
+//
+//     socket.on("createMsg", function (data) {
+//         io.emit("newMsg", data);
+//     });
+// });
+//
+//
+// module.exports = server;
+
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -6,10 +86,9 @@ const router = require("./router");
 const router_adminka = require("./router_adminka");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
-
-let session = require("express-session");
+const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+
 const store = new MongoDBStore({
     uri: process.env.MONGO_URL,
     collection: "session",
@@ -21,8 +100,8 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('js'));
-app.use(cors({credentials: true, origin: true,}));
-app.use(cookieParser()); //
+app.use(cors({credentials: true, origin: "http://localhost:3000"})); // Allow requests from localhost:3000
+app.use(cookieParser());
 
 // 2: Session code
 app.use(
@@ -40,43 +119,43 @@ app.use(
 app.use(function (req, res, next) {
     res.locals.member = req.session.member;
     next();
-})
+});
 
 // 3: Views code
 app.set("views", "views");
-app.set("view engine", "ejs",);
+app.set("view engine", "ejs");
 
 //4: routing code
 app.use("/resto", router_adminka);
 app.use("/", router);
 
-
 const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
-    serveClient: false,
-    origin: "*:*",
-    transport: ["websocket", "xhr-polling"],
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
 });
 
 let online_users = 0;
 io.on("connection", function (socket) {
     online_users++;
-    console.log("New user, total:", online_users);
-    socket.emit("greetMsg", {text: "welcome"});
-    io.emit("infoMsg", {total: online_users});
+    console.log("New user :", online_users);
+    socket.emit("greetMsg", { text: "Welcome" });
+    io.emit("infoUsers", { total: online_users });
 
     socket.on("disconnect", function () {
         online_users--;
-        socket.broadcast.emit("infoMsg", {total: online_users});
-        console.log("client disconnected, total", online_users);
+        socket.broadcast.emit("infoUsers", { total: online_users });
+        console.log("client disconnected, total:", online_users);
     });
 
     socket.on("createMsg", function (data) {
-        console.log("createMsg", data);
         io.emit("newMsg", data);
     });
-})
-
+});
 
 module.exports = server;
